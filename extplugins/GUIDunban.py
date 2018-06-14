a675cb8471197408e7bfa6b84222d31cc6929a42
Miltan <34897878+miltann@users.noreply.github.com>
2018-06-14 16:23:56 +0200

    Updated license

--- extplugins/GUIDunban.py
+++ extplugins/GUIDunban.py
@@ -1,11 +1,31 @@
-# GUIDunban Plugin for BigBrotherBot(B3) (www.bigbrotherbot.com)
-# Copyright (c) 2018 Miltan aka WatchMiltan
 #
-# LOG:
-# 25.05.2018 - v1.0 - WatchMiltan
-# - first release.
-# 05.06.2018 - v1.1 - WatchMiltan
-# - bugfix
+# ################################################################### #
+#                                                                     #
+#  GUIDunban Plugin for BigBrotherBot(B3) (www.bigbrotherbot.com)     #
+#  Copyright (c) 2018 Miltan aka WatchMiltan                          #
+#                                                                     #
+#  This program is free software; you can redistribute it and/or      #
+#  modify it under the terms of the GNU General Public License        #
+#  as published by the Free Software Foundation; either version 2     #
+#  of the License, or (at your option) any later version.             #
+#                                                                     #
+#  This program is distributed in the hope that it will be useful,    #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of     #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the       #
+#  GNU General Public License for more details.                       #
+#                                                                     #
+#  You should have received a copy of the GNU General Public License  #
+#  along with this program; if not, write to the Free Software        #
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA      #
+#  02110-1301, USA.                                                   #
+#                                                                     #
+# ################################################################### #
+#
+#  CHANGELOG:
+#  25.05.2018 - v1.0 - WatchMiltan
+#  - first release.
+#  05.06.2018 - v1.1 - WatchMiltan
+#  - bugfix
 #
 
 __version__ = '1.1gh'
